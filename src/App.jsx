a442236f61e0fa11fc13833ddb23878256871578
Emmanuel Giraldo <esemmanuel2022@gmail.com>
2023-06-04 16:15:20 -0500

    deploy active

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -30,7 +30,7 @@ function App() {
       </p>
       <h2>Designed by Emmanuel S Giraldo</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

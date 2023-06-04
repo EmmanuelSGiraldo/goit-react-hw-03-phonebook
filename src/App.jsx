fdb2e11b27519378df01e958207e76a019dd1044
Emmanuel Giraldo <esemmanuel2022@gmail.com>
2023-06-04 16:08:54 -0500

    project signed

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h2>Designed by Emmanuel S Giraldo</h2>
     </>
   )
 }
